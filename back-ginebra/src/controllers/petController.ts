0aaef6993467a7825a18af36849e978a1394f219
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-08-03 06:09:49 +0200

    petendpoints create and getpublic

---
+++ back-ginebra/src/controllers/petController.ts
@@ -0,0 +1,296 @@
+import { Router, Request, Response } from "express";
+
+import bcrypt from "bcrypt";
+import { generateJwt } from "../helpers/generateJwt";
+import { User } from "../models/User";
+import { Pet } from "../models/Pet";
+import { IPet, IGetUserAuthRequest } from "../interfaces/interfaces";
+import { catchUndefinedError } from "../helpers/utilities";
+
+export const getPublicPets = async (
+  req: IGetUserAuthRequest,
+  res: Response
+) => {
+  try {
+    const pets = await Pet.find({ isPublic: true });
+    return res.status(201).json({
+      ok: true,
+      message: "getPublicPets",
+      pets,
+    });
+  } catch (error) {
+    return catchUndefinedError(error, res);
+  }
+};
+
+export const getAllPets = async (req: IGetUserAuthRequest, res: Response) => {
+  try {
+    const pets = await Pet.find({ isPublic: true });
+    return res.status(201).json({
+      ok: true,
+      message: "getPublicPets",
+      pets,
+    });
+  } catch (error) {
+    return catchUndefinedError(error, res);
+  }
+};
+
+// export const getUsers = async (
+//   req: IGetUserAuthRequest,
+//   res: Response
+// ): Promise<Response> => {
+//   try {
+//     const { userReq } = req;
+//     const { role: userRequestRole, uid: userRequestUid } = userReq!;
+
+//     console.log({ userReq });
+//     const isAuthorized = userRequestRole === "admin";
+//     if (!isAuthorized) {
+//       return res.status(401).json({
+//         ok: false,
+//         msg: "No estás autorizado",
+//       });
+//     }
+
+//     const users = await User.find();
+//     return res.status(201).json({
+//       ok: true,
+//       message: "getUsers",
+//       users,
+//     });
+//   } catch (error) {
+//     const errorMessage = (error as Error).message;
+//     console.log(errorMessage);
+//     return res.status(500).json({
+//       ok: false,
+//       msg: "Hable con el administrador",
+//       error: errorMessage,
+//     });
+//   }
+// };
+
+export const createPet = async (
+  req: IGetUserAuthRequest,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { userReq } = req;
+    const { role: userRequestRole, uid: userRequestUid } = userReq!;
+    const { petName, bathPeriodicity, isPublic } = req.body;
+
+    if (!petName || !bathPeriodicity) {
+      return res.status(400).json({
+        msg: "El nombre de la mascota y la periodicidad del baño son necesarias",
+      });
+    }
+
+    const newPet = new Pet<IPet>({ ...req.body });
+    const savedPet = await newPet.save();
+
+    // TODO LINK THE FKN PET
+    return res.status(201).json({
+      ok: true,
+      message: "Pet created succesfully",
+      savedPet,
+    });
+  } catch (error) {
+    return catchUndefinedError(error, res);
+  }
+};
+
+// export const updateUser = async (
+//   req: IGetUserAuthRequest,
+//   res: Response
+// ): Promise<Response> => {
+//   try {
+//     const { userId } = req.params;
+//     const { userReq } = req;
+//     const { role: userRequestRole, uid: userRequestUid } = userReq!;
+
+//     // check if user exists
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return res.status(404).json({
+//         ok: false,
+//         msg: "No existe usuario con ese ese id",
+//       });
+//     }
+
+//     // the password doesnt change
+//     const { password } = user;
+//     const userNewData = { ...req.body, password };
+
+//     // is authorized to make changes
+//     const isAuthorized =
+//       userRequestRole === "admin" || userRequestUid === userId;
+//     if (!isAuthorized) {
+//       return res.status(401).json({
+//         ok: false,
+//         msg: "No estás autorizado",
+//       });
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(userId, userNewData, {
+//       new: true,
+//     });
+//     return res.status(201).json({
+//       ok: true,
+//       message: "User updated succesfully",
+//       updatedUser,
+//     });
+//   } catch (error) {
+//     const errorMessage = (error as Error).message;
+//     console.log(errorMessage);
+//     return res.status(500).json({
+//       ok: false,
+//       msg: "Hable con el administrador",
+//       error: errorMessage,
+//     });
+//   }
+// };
+
+// export const changePassword = async (
+//   req: IGetUserAuthRequest,
+//   res: Response
+// ): Promise<Response> => {
+//   try {
+//     const { userId } = req.params;
+//     const { userReq } = req;
+//     const { role: userRequestRole, uid: userRequestUid } = userReq!;
+
+//     // check if user exists
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return res.status(404).json({
+//         ok: false,
+//         msg: "No existe usuario con ese ese id",
+//       });
+//     }
+
+//     // TODO check previous password
+
+//     // the password doesnt change
+//     const { newPassword } = req.body;
+//     // encrypt pass
+//     const salt = bcrypt.genSaltSync();
+//     const cryptedPassword = bcrypt.hashSync(newPassword, salt);
+
+//     const userNewData = { ...req.body, password: cryptedPassword };
+
+//     // is authorized to make changes
+//     const isAuthorized =
+//       userRequestRole === "admin" || userRequestUid === userId;
+//     if (!isAuthorized) {
+//       return res.status(401).json({
+//         ok: false,
+//         msg: "No estás autorizado",
+//       });
+//     }
+
+//     const updatedUser = await User.findByIdAndUpdate(userId, userNewData, {
+//       new: true,
+//     });
+//     return res.status(201).json({
+//       ok: true,
+//       message: "User updated succesfully",
+//       updatedUser,
+//     });
+//   } catch (error) {
+//     const errorMessage = (error as Error).message;
+//     console.log(errorMessage);
+//     return res.status(500).json({
+//       ok: false,
+//       msg: "Hable con el administrador",
+//       error: errorMessage,
+//     });
+//   }
+// };
+
+// export const userLogin = async (req: Request, res: Response) => {
+//   try {
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email });
+//     let isValid = false;
+//     if (user) {
+//       const validPassword = bcrypt.compareSync(password, user.password);
+//       isValid = validPassword ? true : false;
+//     }
+
+//     if (!isValid) {
+//       return res.status(400).json({
+//         ok: false,
+//         msg: "Email o contraseña incorrecta",
+//       });
+//     }
+
+//     // Generate JWT
+//     const token = await generateJwt(
+//       user!._id.toString(),
+//       user!.username,
+//       user!.email,
+//       user!.role
+//     );
+
+//     return res.status(201).json({
+//       ok: true,
+//       message: "Succesfully login",
+//       token,
+//       user,
+//     });
+//     // check if collaborator or user
+//     // let user = await Collaborator.findOne({ email });
+//     // let userType = "collaborator";
+//     // if (!user) {
+//     //   user = await User.findOne({ email });
+//     //   userType = "user";
+//     // }
+
+//     // let isValid = false;
+
+//     // if (user) {
+//     //   const validPassword = bcrypt.compareSync(password, user.password);
+//     //   if (validPassword) {
+//     //     isValid = true;
+//     //   }
+//     // }
+
+//     // if (!isValid) {
+//     //   return res.status(400).json({
+//     //     ok: false,
+//     //     msg: "Email o contraseña incorrecta",
+//     //   });
+//     // }
+
+//     // Generate JWT
+//     // const token = await generateJWT(
+//     //   user.id,
+//     //   user.col_code,
+//     //   user.role,
+//     //   user.imgUrl
+//     // );
+
+//     // generate the log
+//     // registerLog(userType, user, authTypes.login);
+
+//     // res.json({
+//     //   ok: true,
+//     //   uid: user.id,
+//     //   token,
+//     //   col_code: user.col_code,
+//     //   role: user.role,
+//     //   imgUrl: user.imgUrl,
+//     // });
+
+//     res.json({
+//       ok: true,
+//       uid: "wa",
+//       token: "wa",
+//       col_code: "wa",
+//       role: "wa",
+//       imgUrl: "wa",
+//     });
+//   } catch (error) {
+//     // uncatchedError(error, res);
+//   }
+// };
---
+++ back-ginebra/src/routes/petRoutes.ts
@@ -0,0 +1,148 @@
+import express, { Router, Request, Response } from "express";
+import {
+  userLogin,
+  createUser,
+  updateUser,
+  getUsers,
+} from "../controllers/authController";
+import { check } from "express-validator";
+import { fieldValidator } from "../middlewares/fieldValidator";
+import { validateJwt } from "../helpers/validateJwt";
+import { changePassword } from "../controllers/authController";
+import { createPet, getPublicPets } from "../controllers/petController";
+
+export const petRoutes = Router();
+
+// TODO: getPublicPets
+// TODO: getAllPets
+// TODO: createPet
+// TODO: updatePet
+// TODO: deletePet
+// TODO: crudBath
+
+petRoutes.get("/getPublicPets", getPublicPets);
+petRoutes.get("/getAllPets", validateJwt, getAllPets);
+petRoutes.post(
+  "/createPet",
+  [
+    check("petName", "El nombre de la mascota es necesario").isLength({
+      min: 2,
+    }),
+    check(
+      "bathPeriodicity",
+      "La periodicidad de los baños es necesaria"
+    ).notEmpty(),
+    fieldValidator,
+  ],
+  validateJwt,
+  createPet
+);
+// // route called by the callback if its a success
+// router.get("/googleLogin/success", (req, res) => {
+//   console.log("success", req.user);
+//   if (req.user) {
+//     res.status(200).json({
+//       success: true,
+
+//       message: "successfull",
+//       user: req.user,
+//       token: req.user.token,
+//       //   cookies: req.cookies
+//     });
+//   }
+// });
+
+// // route called by the callback if its a failure
+// router.get("/googleLogin/failed", (req, res) => {
+//   res.status(401).json({
+//     success: false,
+//     message: "failure",
+//   });
+// });
+
+// // first call from client, it triggers passport to very google account
+// router.get(
+//   "/google",
+//   passport.authenticate("google", { scope: ["profile", "email"] })
+// );
+
+// // callback from google
+// router.get(
+//   "/google/callback",
+//   passport.authenticate("google", {
+//     failureRedirect: "/api/auth/googleLogin/failed",
+//     // successRedirect: `${process.env.CLIENT_URL}/#/auth`,
+//   }),
+//   googleAuth
+// );
+
+/************USERS CRUD********* */
+
+// // GET USERS
+// authRoutes.get("/allUsers", validateJwt, getUsers);
+
+// // USER LOGIN
+// authRoutes.post("/login", userLogin);
+// // CREATE USER
+// authRoutes.post(
+//   "/createUser",
+//   [
+//     check(
+//       "username",
+//       "El nombre de usuario debe tener entre 4 y 16 caracteres"
+//     ).isLength({ min: 4, max: 16 }),
+//     check("email", "no es una forma de email correcta").isEmail(),
+//     fieldValidator,
+//   ],
+//   createUser
+// );
+
+// // UPDATE USER
+// authRoutes.put(
+//   "/updateUser/:userId",
+//   [
+//     check(
+//       "username",
+//       "El nombre de usuario debe tener entre 4 y 16 caracteres"
+//     ).isLength({ min: 4, max: 16 }),
+//     check("email", "no es una forma de email correcta").isEmail(),
+//     fieldValidator,
+//   ],
+//   validateJwt,
+//   updateUser
+// );
+
+// // CHANGE PASSWORD
+// authRoutes.patch(
+//   "/changepass/:userId",
+//   [
+//     check("previousPassword", "El password anterir es necesario").notEmpty(),
+//     check("newPassword", "El password anterir es necesario").notEmpty(),
+//     fieldValidator,
+//   ],
+//   validateJwt,
+//   changePassword
+// );
+
+// // // LOGIN
+// // router.post(
+// //   "/",
+// //   [
+// //     check("email", "no es una forma de email correcta").isEmail(),
+// //     fieldValidator,
+// //   ],
+// //   userLogin
+// // );
+
+// // // TODO: XXXX
+// // router.get("/logout", (req, res) => {
+// //   req.logout();
+// //   res.redirect(`${process.env.CLIENT_URL}`);
+// // });
+
+// // // renew token
+// // router.get("/renew", validateJwt, userRenewToken);
+
+// // router.get("test", (req, res) => {});
+
+// // module.exports = router;
